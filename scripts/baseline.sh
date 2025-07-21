349541e1f56709846344563d50687bffe2d2bdce
dhatchet <dhatchet@redhat.com>
2025-07-21 11:03:39 +1000

    bugfix

--- scripts/baseline.sh
+++ scripts/baseline.sh
@@ -73,15 +73,15 @@ for _J_CUTSPEC in ${_NDJSON_CUTSPEC[@]}; do
   declare -A A1
   declare L1
 
-  _f_parse_cutspec $_J_CUTSPEC _CATALOG_UPSTREAM A1 L1
+  _f_catalog_baseline _J_CUTSPEC _CATALOG_BASELINE 
 
   # catalog_baseline: registry.redhat.io/redhat/redhat-operator-index:v4.18 
   # targetCatalog: reg.dmz.lan/baseline/20250709/redhat-operator-index:v4.18
 
-  _NAMETAG=$(basename $_CATALOG_UPSTREAM)
+  _NAMETAG=$(basename $_CATALOG_BASELINE)
   _CATALOG_TARGET=$_REG_LOCATION/$_DATESTAMP/${_NAMETAG}
 
-  read _INDEX_LOCATION _INDEX_NAME _TAG <<<$(_f_indexname_tag $_CATALOG_UPSTREAM)
+  read _INDEX_LOCATION _INDEX_NAME _TAG <<<$(_f_indexname_tag $_CATALOG_BASELINE)
  
   _CATALOG_BASELINE=$_REG_LOCATION/$_DATESTAMP/${_NAMETAG}
 
@@ -89,9 +89,9 @@ for _J_CUTSPEC in ${_NDJSON_CUTSPEC[@]}; do
   _log 2 "(baseline.sh) _CATALOG_BASELINE: $_CATALOG_BASELINE"
 
   _log 1 "(baseline.sh) _f_get_image $_INDEX_LOCATION $_TAG $_CATALOG_BASELINE"
-  _f_get_image $_INDEX_LOCATION $_TAG $_CATALOG_UPSTREAM
+  _f_get_image $_INDEX_LOCATION $_TAG $_CATALOG_BASELINE
 
-  _f_pod_tag $_CATALOG_UPSTREAM $_CATALOG_BASELINE
+  _f_pod_tag $_CATALOG_BASELINE $_CATALOG_BASELINE
 
   [[ -z $_SKIP_PUSH ]] && _f_pod_push $_CATALOG_BASELINE
 
